9aa6459ce8c422bc624f4296cd24e8ceebdfb501
Joshiwoshi1 <joshuahao987@gmail.com>
2024-01-24 12:07:57 -0800

    Added "MainMenu"

---
+++ Assets/Josh/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private string startScene;
+
+
+    //start button
+    //transition into test scene
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+
+
+
+    //exit button will close application
+
+    public void EndGame()
+    {
+        Application.Quit();
+        print("Quit Game");
+    }
+
+
+
+
+
+
+
+}
