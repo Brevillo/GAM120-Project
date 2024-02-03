2eadd8cf231d78dae991aef6bafb8c84e2f7d658
Joshiwoshi1 <joshuahao987@gmail.com>
2024-02-03 00:52:46 -0800

    Blue bird thingy

---
+++ Assets/Enemies/Hummingbirds/Blue/BlueHummingbird.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class BlueHummingbird : GenericHummingbird
+{
+    [Header("Attacking")]
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private float attackAcceleration;
+    [SerializeField] private float attackDuration;
+    protected override IEnumerator Attack()
+    {
+        float timer = 0;
+        while (timer < attackDuration)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+            print(timer);
+            Vector2 toTarget = (TargetPosition - Position).normalized;
+            Vector2 targetVelocity = toTarget * attackSpeed;
+
+            Velocity = Vector2.MoveTowards(Velocity, targetVelocity, attackAcceleration * Time.deltaTime);
+            
+        }
+    }
+
+
+}
