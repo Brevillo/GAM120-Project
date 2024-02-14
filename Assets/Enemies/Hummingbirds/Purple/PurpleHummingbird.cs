b75d7014e8bdd2a387fe280d84cae274e8962c6b
Joshiwoshi1 <joshuahao987@gmail.com>
2024-02-14 13:49:58 -0800

    Script

--- Assets/Enemies/Hummingbirds/Purple/PurpleHummingbird.cs
+++ Assets/Enemies/Hummingbirds/Purple/PurpleHummingbird.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PurpleHummingbird : GenericHummingbird
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Idling")]
+    [SerializeField] private float minIdleTime;
+    [SerializeField] private float maxIdleTime, airFriction;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Babies")]
+    [SerializeField] private GameObject miniPurple;
+
+    [Header("Diving")]
+    [SerializeField] private float diveSpeed;
+    [SerializeField] private float maxDiveSpeed, playerPassDistance;
+
+    // Start is called before the first frame update
+    public override IWhippable.Type WhippableType => IWhippable.Type.Light;
+
+    protected IEnumerator Idle()
     {
-        
+
+        float idleTime = Random.Range(minIdleTime, maxIdleTime);
+
+        while (idleTime > 0)
+        {
+
+            idleTime -= Time.deltaTime;
+
+            // move velocity to zero if bumped
+            Velocity = Vector2.MoveTowards(Velocity, Vector2.zero, airFriction * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+    protected override IEnumerator Attack()
+    {
+        yield return null;
+        /*Vector2 diveVector = TargetPosition - Position,
+                diveDirection = diveVector.normalized,
+                divePosition = diveDirection * (Mathf.Min(diveVector.magnitude, maxDiveDist) + playerPassDistance) + Position;
+
+        yield return MoveTo(divePosition, diveSpeed);
+
+        Velocity = Vector2.zero;*/
     }
 }
