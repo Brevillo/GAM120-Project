82dd8d378e326c8325d6e159f7797d740e4b162a
Brevillo <oliver.beebe@digipen.edu>
2024-04-05 15:14:16 -0700

    Main menu rework

--- Assets/Menus/Main Menu/MainMenu.cs
+++ Assets/Menus/Main Menu/MainMenu.cs
@@ -6,30 +6,40 @@ using OliverBeebe.UnityUtilities.Runtime;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Scene startScene;
+    [SerializeField] private GameObject settingsContent;
+    [SerializeField] private GameObject creditsContent;
 
+    private enum State { None, Settings, Credits }
+    private State state;
 
-    //start button
-    //transition into test scene
     public void StartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(startScene);
     }
 
+    private void ChangeState(State newState)
+    {
+        state = state != newState
+            ? newState
+            : State.None;
 
+        settingsContent.SetActive(state == State.Settings);
+        creditsContent.SetActive(state == State.Credits);
+    }
 
+    public void ToggleSettings()
+    {
+        ChangeState(State.Settings);
+    }
 
-    //exit button will close application
+    public void ToggleCredits()
+    {
+        ChangeState(State.Credits);
+    }
 
-    public void EndGame()
+    public void QuitGame()
     {
         Application.Quit();
         print("Quit Game");
     }
-
-
-
-
-
-
-
 }
